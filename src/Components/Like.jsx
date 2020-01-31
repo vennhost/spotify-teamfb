a8c4841e6f916fd12addf77ebcf52ea72a007061
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-31 15:42:32 +0100

    removed react-simple-storage

--- src/Components/Like.jsx
+++ src/Components/Like.jsx
@@ -1,33 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Row, Col, Card, Container, Button } from "react-bootstrap";
-import SimpleStorage from "react-simple-storage";
 
 const LikeButton = () => {
-    const [value, setValue] = React.useState('Like');
-        const onClick = () => {
-            localStorage.setItem('myValueInLocalStorage', 'Liked');
-            setValue('Liked');
-            localStorage.getItem('Liked')
-          };
-    return (
-        <div>
-                
-        <button onClick={onClick}>{value}</button>
-        
-    </div>
-    );
+  const [value, setValue] = React.useState("Like");
+  const onClick = () => {
+    localStorage.setItem("myValueInLocalStorage", "Liked");
+    setValue("Liked");
+    localStorage.getItem("Liked");
   };
-  export default LikeButton;
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return (
+    <div>
+      <button onClick={onClick}>{value}</button>
+    </div>
+  );
+};
+export default LikeButton;
